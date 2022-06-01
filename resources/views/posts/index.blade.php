650eda73002f18520a2600599be99533e9dd57bd
memee46 <wasita.gsp@gmail.com>
2022-06-01 22:29:24 +0700

    second commit

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,47 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Posts') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <x-validation-errors class="mb-4" :errors="$errors" />
+                    <x-success-message />
+                    <div class="flex justify-end">
+                        <a href="{{ route('posts.create') }}">Create Post</a>
+                    </div>
+                    <table class="min-w-full divide-y divide-gray-200">
+                        <thead class="bg-gray-50">
+                            <tr>
+                                <th>
+                                    ID
+                                </th>
+                                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Message</th>
+                                <th>User</th>
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody class="bg-white divide-y divide-gray-200">
+                            @foreach ($posts as $post)
+                                <tr class="border border-collapse border-slate-800">
+                                    <td class="border"> {{ $post->id }} </td>
+                                    <td class="border"> {{ $post->body }} </td>
+                                    <td class="border"> {{ $post->owner->name }} </td>
+                                    <td class="border"> 
+                                        <a href="{{ route('posts.show', $post) }}">View</a>
+                                        <a href="{{ route('posts.edit', $post) }}">Edit</a>
+                                        <a href="{{ route('posts.delete', ['post' => $post]) }}">Delete</a>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
